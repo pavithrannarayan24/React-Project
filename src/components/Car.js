2c686886e5afdc738b1acb807bffbcc43afc7ddf
pavithrannarayan24 <pavithrannarayan24@gmail.com>
2025-07-25 22:14:01 +0530

    Initial commit

---
+++ src/components/Car.js
@@ -0,0 +1,7 @@
+function Car(){
+  return (
+    <h2>hi i am car</h2>
+  );
+}
+
+export default Car;
---
+++ src/components/Garage.js
@@ -0,0 +1,14 @@
+import Car from './Car';
+function Garage(){
+  return (
+   
+    <>
+      <h1>who lives inside my Garage?</h1>
+    <Car/>
+    </>
+    
+   
+  );
+}
+
+export default Garage;
